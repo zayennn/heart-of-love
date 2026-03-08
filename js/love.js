185caf89f7542557c9005c663392c7cbcb853e28
zayennn <zaayeennn@gmail.com>
2026-03-09 06:44:56 +0700

    update

--- js/love.js
+++ js/love.js
@@ -1,28 +1,28 @@
 const blk_pitn = {
-        block1: [[0, 1], [0, 0], [-1, 0], [-1, -1]],
-        block2: [[0, 1], [0, 0], [-1, 0], [0, -1]],
-        block3: [[-1, 1], [0, 0], [-1, 0], [-1, -1]],
-        block4: [[0, 1], [0, 0], [-1, 0], [-1, -1]], /* 1 */
-        block5: [[-1, 1], [0, 0], [-1, 0], [0, -1]],
-        block6: [[0, -1], [0, 0], [-1, 0], [1, -1]],
-        block7: [[-1, -1], [0, 0], [-1, 0], [1, 0]],
-        block8: [[-1, 1], [0, 0], [-1, 0], [-1, -1]], /* 3 */
-        block9: [[0, -1], [0, 0], [-1, 0], [1, 0]],
-        block10: [[-1, 1], [0, 0], [-1, 0], [1, 0]],
-        block11: [[2, 0], [0, 0], [-1, 0], [1, 0]], /* — */
-        block12: [[0, 1], [0, 0], [-1, 0], [0, -1]], /* 2 */
-        block13: [[0, 1], [0, 0], [-1, 0], [-1, -1]], /* 1 */
-        block14: [[1, 1], [0, 0], [-1, 0], [1, 0]],
-        block15: [[1, -1], [0, 0], [-1, 0], [1, 0]],
-        block16: [[-1, -1], [0, 0], [-1, 0], [1, 0]], /* 7 */
-        block17: [[0, 1], [0, 0], [-1, 0], [0, -1]], /* 2 */
-        block18: [[0, 1], [0, 0], [-1, 0], [-1, -1]], /* 1 */
-        block19: [[0, -1], [0, 0], [-1, 0], [1, 0]], /* 9 */
-        block20: [[1, -1], [0, 0], [-1, 0], [1, 0]],
-        block21: [[0, 1], [0, 0], [-1, 0], [-1, -1]], /* 1 */
-        block22: [[1, 1], [0, 0], [-1, 0], [1, 0]], /* 14 */
-        block23: [[0, 2], [0, 0], [0, -1], [0, 1]]      /* | */
-    },
+    block1: [[0, 1], [0, 0], [-1, 0], [-1, -1]],
+    block2: [[0, 1], [0, 0], [-1, 0], [0, -1]],
+    block3: [[-1, 1], [0, 0], [-1, 0], [-1, -1]],
+    block4: [[0, 1], [0, 0], [-1, 0], [-1, -1]], /* 1 */
+    block5: [[-1, 1], [0, 0], [-1, 0], [0, -1]],
+    block6: [[0, -1], [0, 0], [-1, 0], [1, -1]],
+    block7: [[-1, -1], [0, 0], [-1, 0], [1, 0]],
+    block8: [[-1, 1], [0, 0], [-1, 0], [-1, -1]], /* 3 */
+    block9: [[0, -1], [0, 0], [-1, 0], [1, 0]],
+    block10: [[-1, 1], [0, 0], [-1, 0], [1, 0]],
+    block11: [[2, 0], [0, 0], [-1, 0], [1, 0]], /* — */
+    block12: [[0, 1], [0, 0], [-1, 0], [0, -1]], /* 2 */
+    block13: [[0, 1], [0, 0], [-1, 0], [-1, -1]], /* 1 */
+    block14: [[1, 1], [0, 0], [-1, 0], [1, 0]],
+    block15: [[1, -1], [0, 0], [-1, 0], [1, 0]],
+    block16: [[-1, -1], [0, 0], [-1, 0], [1, 0]], /* 7 */
+    block17: [[0, 1], [0, 0], [-1, 0], [0, -1]], /* 2 */
+    block18: [[0, 1], [0, 0], [-1, 0], [-1, -1]], /* 1 */
+    block19: [[0, -1], [0, 0], [-1, 0], [1, 0]], /* 9 */
+    block20: [[1, -1], [0, 0], [-1, 0], [1, 0]],
+    block21: [[0, 1], [0, 0], [-1, 0], [-1, -1]], /* 1 */
+    block22: [[1, 1], [0, 0], [-1, 0], [1, 0]], /* 14 */
+    block23: [[0, 2], [0, 0], [0, -1], [0, 1]]      /* | */
+},
     offset_pitn = {
         block1: [5, 3],
         block2: [5, 1],
@@ -35,7 +35,7 @@ const blk_pitn = {
         block9: [1, -3],
         block10: [1, 2],
         block11: [0, 3],
-        block12: [0, 0], 
+        block12: [0, 0],
         block13: [-1, -4],
         block14: [0, -2],
         block15: [-2, 4],
@@ -46,15 +46,15 @@ const blk_pitn = {
         block20: [-3, 5],
         block21: [-5, 3],
         block22: [-4, 1],
-        block23: [-6, 1]   
+        block23: [-6, 1]
     };
 
 let blocks = document.getElementsByClassName("block"),
     block = blocks[0],
     love = document.getElementsByClassName("love")[0],
     timer = null,
-    index = 0, 
-    clone_block;    
+    index = 0,
+    clone_block;
 
 block.style.top = "50%";
 block.style.left = "50%";
@@ -71,7 +71,7 @@ function Next() {
         return;
     }
 
-    block.style.visibility = "visible"; 
+    block.style.visibility = "visible";
 
     block.style.left = block_left + 40 * offset_pitn["block" + index][0] + "px";
     block.style.top = block_top - 40 * offset_pitn["block" + index][1] + "px";
@@ -85,7 +85,7 @@ function Next() {
 
     if (love.children.length >= 24) {
         blocks[blocks.length - 1].children[2].style.display = "none";
-        block.style.display = "none";   
+        block.style.display = "none";
     }
 }
 
@@ -93,7 +93,7 @@ function Rise() {
     console.log("开始升空");
     let timer2 = null,
         distance = 0;
-    const target = 120, 
+    const target = 120,
         speed = 1;
 
     let love_top = parseFloat(window.getComputedStyle(love, null).top.slice(0, -2));
@@ -104,9 +104,6 @@ function Rise() {
         // console.log(distance);
         if (distance >= target) {
             clearInterval(timer2);
-
-            console.log("升空完毕");
-
         }
 
         love.style.top = (love_top - distance) + "px";
